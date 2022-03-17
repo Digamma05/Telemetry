011e1e132a7a22c2458bef73eb16487a50be7a87
Joseph Coppin <49319713+revers3ntropy@users.noreply.github.com>
2022-03-17 18:48:03 +0000

    Update main.py
    code cleanup - not tested

--- main.py
+++ main.py
@@ -7,31 +7,42 @@ import adafruit_adxl34x
 i2c = board.I2C()
 accelerometer = adafruit_adxl34x.ADXL343(i2c)
 
-t = 0.01 # time
-a = accelerometer.acceleration # input from accelerometer
-v = [0, 0, 0] # final velocity
-u = [0, 0, 0] # initial velocity
-run = 1
+ # time
+t = 0.01
+# input from accelerometer
+a = accelerometer.acceleration
+# velocity
+v = [0, 0, 0]
 
-x = 0
 sample = 1000
 cal = [0, 0, 0]
 
-while x != sample:
-  cal = [
-    (cal[0] + a[0]) / sample,
-    (cal[1] + a[1]) / sample,
-    (cal[2] + a[2]) / sample
-  ]
-  x = x + 1
-  print(cal)
+def calibrate():
+  for _ in range(sample):
+    cal = [
+      (cal[0] + a[0]) / sample,
+      (cal[1] + a[1]) / sample,
+      (cal[2] + a[2]) / sample
+    ]
+    print(cal)
+    
+def run():
+  while True:
+    # refresh data from accelerometer
+    a = accelerometer.acceleration
 
-while run == 1:
-  v = [
-    u[0]+(a[0]-cal[0])*t,
-    u[1]+(a[1]-cal[1])*t,
-    u[2]+(a[2]-cal[2])*t
-  ]
-  u = v
-  print(v)
-  time.sleep(t)
+    v = [
+      v[0]+(a[0]-cal[0])*t,
+      v[1]+(a[1]-cal[1])*t,
+      v[2]+(a[2]-cal[2])*t
+    ]
+    
+    print(v)
+    time.sleep(t)
+    
+def main():
+  calibrate()
+  run()
+  
+if __name__ == '__main__':
+  main()
